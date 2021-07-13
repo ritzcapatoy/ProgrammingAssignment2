5ef10b000b926fb903ef1cd2ae335629ccf29038
ritzcapatoy <87307765+ritzcapatoy@users.noreply.github.com>
2021-07-13 21:28:04 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a pair of functions that cache the mean of a matrix
+## To use: Deliver the result of makeCacheMatrix call to cacheSolve
 
-## Write a short comment describing this function
+## @param x is an invertible matrix
+## Example: 
+## x = makeCacheMatrix(matrix(rnorm(9), 3, 3)), and
+## x$set(matrix(rnorm(16), 4, 4))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # todo error if x is not a matrix
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## Compute and cache the mean of a matrix
+## @param x is the result of the previous makeCacheMatrix call
+## @param ... additional arguments to pass to solve function
+## Example:
+## x = makeCacheMatrix(matrix(rnorm(9), 3, 3))
+## cacheSolve(x)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the mean of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmean(m)
+  m
 }
